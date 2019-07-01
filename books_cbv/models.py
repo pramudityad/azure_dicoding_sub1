3ae4300b3abffb87a95d022629395a42021bd0f7
Damar Pramuditya <pramuditayd@yahoo.com>
2019-07-01 22:30:58 +0700

    def #2

--- books_cbv/models.py
+++ books_cbv/models.py
@@ -5,6 +5,8 @@ from django.urls import reverse
 class Book(models.Model):
     name = models.CharField(max_length=200)
     pages = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True, null=True, blank=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True, blank=True)
 
     def __str__(self):
         return self.name
