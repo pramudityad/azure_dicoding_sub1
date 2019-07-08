9f21eacf895e686a756194b593d1ea9f8aafc3ce
Damar Pramuditya <pramuditayd@yahoo.com>
2019-07-08 15:13:13 +0700

    change static_url

--- apps/settings.py
+++ apps/settings.py
@@ -137,11 +137,11 @@ USE_TZ = True
 
 STATIC_ROOT = 'staticfiles'
 
-STATIC_URL = '/static/'
+STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
 
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 
 ]
 
